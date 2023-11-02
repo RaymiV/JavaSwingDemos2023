aa3e3e543ba99885a8b0148274c1e70986270b3a
Prakash Manandhar <jsaaymi@gmail.com>
2023-11-02 13:44:57 -0400

    Add timer to section 19 code

--- src/HelloGraphics2D19.java
+++ src/HelloGraphics2D19.java
@@ -1,10 +1,16 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 public class HelloGraphics2D19 
-extends JFrame
+extends JFrame implements ActionListener
 {
-	private static final long serialVersionUID = 2797131981717940062L;
+	private static final long serialVersionUID = -7136167299427637163L;
 	private HelloGraphics2DPanel19 panel;
+	private Timer timer;
+	private long timer_i = 0;
 	
 	
 	public HelloGraphics2D19() {
@@ -12,11 +18,21 @@ extends JFrame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		panel = new HelloGraphics2DPanel19();
 		add(panel);
+		
+		timer = new Timer(1000, this);
+		
 		setSize(750, 750);
+		timer.start();
 	}
 
 	public static void main(String[] args) {
 		new HelloGraphics2D19().setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Hello Timer " + timer_i);
+		timer_i++;
 	}
 
 }
