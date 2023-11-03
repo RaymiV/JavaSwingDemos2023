ab0548e66915e1a610e381721abfef60b391c426
Prakash Manandhar <jsaaymi@gmail.com>
2023-11-03 17:33:13 -0400

    Added Timer Section 07

--- src/HelloAnimationDemo07.java
+++ src/HelloAnimationDemo07.java
@@ -1,21 +1,36 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 public class HelloAnimationDemo07 
-extends JFrame
+extends JFrame implements ActionListener
 {
 	private static final long serialVersionUID = -2245613981181293044L;
 	private SimplePanel07 panel;
+	private Timer timer;
+	private int timer_i = 0;
 
 	public HelloAnimationDemo07() {
 		super("Simple Animation Demo Section 07");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		panel = new SimplePanel07();
 		this.add(this.panel);
+		timer = new Timer(1000, this);
+		timer.setInitialDelay(2000);
 		setSize(500, 400);
+		timer.start();
 	}
 
 	public static void main(String [] args) {
 		new HelloAnimationDemo07().setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Hello Timer " + timer_i);
+		timer_i++;
 	}
 	
 }
