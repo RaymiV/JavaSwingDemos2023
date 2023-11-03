bc67b2ad0d446cc91f0daef32dcaed9518cabf66
Prakash Manandhar <jsaaymi@gmail.com>
2023-11-03 17:59:38 -0400

    Animated single ball Section 07

--- src/HelloAnimationDemo07.java
+++ src/HelloAnimationDemo07.java
@@ -10,16 +10,16 @@ extends JFrame implements ActionListener
 	private static final long serialVersionUID = -2245613981181293044L;
 	private SimplePanel07 panel;
 	private Timer timer;
-	private int timer_i = 0;
+//	private int timer_i = 0;
 
 	public HelloAnimationDemo07() {
 		super("Simple Animation Demo Section 07");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		panel = new SimplePanel07();
 		this.add(this.panel);
-		timer = new Timer(1000, this);
+		timer = new Timer(10, this);
 		timer.setInitialDelay(2000);
-		setSize(500, 400);
+		setSize(700, 700);
 		timer.start();
 	}
 
@@ -29,8 +29,8 @@ extends JFrame implements ActionListener
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("Hello Timer " + timer_i);
-		timer_i++;
+//		System.out.println("Hello Timer " + timer_i);
+//		timer_i++;
 		panel.tickTime();
 		panel.repaint();
 	}
--- src/SimplePanel07.java
+++ src/SimplePanel07.java
@@ -6,16 +6,35 @@ extends JPanel
 {
 
 	private static final long serialVersionUID = 3569086350820821753L;
-	private int pos_x = 10, pos_y = 10;
+	private double pos_x = 10, pos_y = 10;
+	private double vel_x = 1.0, vel_y = 0.5;
+	private static final double BALL_DIAMETER = 25.0;
 	
 	public void tickTime() {
-		pos_x += 10;
-		pos_y += 10;
+		pos_x += vel_x;
+		pos_y += vel_y;
+		if((pos_x + BALL_DIAMETER/2) > this.getWidth()) {
+			vel_x = -vel_x;
+			pos_x = this.getWidth() - BALL_DIAMETER/2;
+		}
+		if((pos_y + BALL_DIAMETER/2) > this.getHeight()) {
+			vel_y = -vel_y;
+			pos_y = this.getHeight() - BALL_DIAMETER/2;
+		}
+		if((pos_x - BALL_DIAMETER/2) < 0) {
+			vel_x = -vel_x;
+			pos_x = BALL_DIAMETER/2;
+		}
+		if((pos_y - BALL_DIAMETER/2) < 0) {
+			vel_y = -vel_y;
+			pos_y = BALL_DIAMETER/2;
+		}
 	}
 
 	public void paint(Graphics g) {
 		g.clearRect(0, 0, this.getWidth(), this.getHeight());
-		g.drawLine(pos_x, pos_y, pos_x + 100, pos_y + 100);
+		g.fillOval((int)Math.round(pos_x), (int)Math.round(pos_y), 
+				(int)BALL_DIAMETER, (int)BALL_DIAMETER);
 	}
 	
 }
