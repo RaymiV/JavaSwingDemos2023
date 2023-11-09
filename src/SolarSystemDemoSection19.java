b03408ecc742f5683f771463bbca5f590b15ab21
Prakash Manandhar <jsaaymi@gmail.com>
2023-11-09 13:49:11 -0500

    Bootstramp Solar System Demo Section 19

---
+++ src/SolarSystemDemoSection19.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+public class SolarSystemDemoSection19 
+extends JFrame
+{
+
+	private static final long serialVersionUID = 7038370709677235888L;
+
+	public SolarSystemDemoSection19() {
+		super("Solar System Animation Demo Section 19");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		new SolarSystemDemoSection19().setVisible(true);
+	}
+
+}
